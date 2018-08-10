76591dcfb5872abd9d8c9e2e1481635bf4a037c4
hafeez <hafeez.cs@gmail.com>
2018-08-10 12:43:43 +0800

    Basic C file

---
+++ skeleton.c
@@ -0,0 +1,18 @@
+/*
+Filename: skeleton.c
+Author : myapit
+E-mail : 9w2xhr@gmail.com
+Purpose: Basic skeleton of C code
+Compiling : gcc -o skeleton skeleton.c
+Runnning : ./skeleton
+*/
+
+#include <stdio.h> /* Standard Input Output */
+#include <stdlib.h> /* Standard library */
+
+int main( int argc, char *argv[])
+{
+
+  return 0;
+}
+ 
