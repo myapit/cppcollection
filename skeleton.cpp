77efbcc202a5dc9c4b1304b0a72a21f580bbca49
hafeez <hafeez.cs@gmail.com>
2018-08-10 12:49:13 +0800

    Basic C++ code skeleton

---
+++ skeleton.cpp
@@ -0,0 +1,18 @@
+/*
+Filename: skeleton.cpp
+Author : myapit
+E-mail : 9w2xhr@gmail.com
+Purpose: Basic skeleton of C++ code
+Compiling : g++ -o skeleton skeleton.cpp
+Runnning : ./skeleton
+*/
+
+#include <iostream>
+
+using namespace std; /* To write code cout instead of std::cout .. use with caution for large code */
+
+int main(int argc, char *argv[])
+{
+	cout << "Hi!" << endl;
+	return 0;
+}
