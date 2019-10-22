dc1c05129183aaba8314c6aea0bded5bb71d2ee8
myapit <hafeez.cs@gmail.com>
2019-10-22 13:21:46 +0800

    add example namespace with class.

--- linux_programming/namespace-c.cpp
+++ linux_programming/namespace-c.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <string>
 
-
+// Namespace with class
 namespace nsOne {
     class myObjOne {
         private:
@@ -25,14 +25,75 @@ namespace nsOne {
     };
 }
 
+//namespace with class and method outside namespace
+namespace nsTwo {
+    class Vehical {
+        private:
+            int engineCapacity;
+            std::string engineType;
+
+         public:
+            void setVehicalData();
+            void displayData();
+    };
+}
+
+void nsTwo::Vehical::setVehicalData()
+{
+    std::cout << "Setting Engine Capacity  (cc) and Engine Type (diesel, petrol, electric)." << std::endl;
+    engineCapacity=1200;
+    engineType="Petrol";
+}
+
+void nsTwo::Vehical::displayData()
+{
+    std::cout << "This car have cc of :" << engineCapacity << std::endl;
+    std::cout << "This car use " << engineType << " as it fuel." << std::endl;
+}
+
+//namespace with class oside
+
+namespace nsThree {
+        class animal;
+}
+
+class nsThree::animal {
+    private:
+         std::string animalName;
+
+    public:
+         void displayAnimal()
+         {
+             animalName = "Harimau";
+             std::cout << "Animal name Harimau." << std::endl;
+         }
+
+         static void kucing()
+         {
+             std::cout << "it kucing.." << std::endl;
+         }
+};
+
+/*
+ * Main Program
+ */
+
+
 using namespace std;
+
 int main()
 {
     nsOne::myObjOne useObj;
-    
     cout << "Namespace Object Ouput :" << useObj.sayHello() << endl;
     cout << "NS int output:" << useObj.lengthHello() << endl;
+     
+    nsTwo::Vehical car;
+    car.setVehicalData();
+    car.displayData();
+   
+    nsThree::animal ani;
+    ani.displayAnimal();
+    nsThree::animal::kucing();
 
-    std::cout << "Press any key to exit .. ";
-	std::cin.get();
+    std::cout << "--Program Terminated--" << std::endl;
 }
