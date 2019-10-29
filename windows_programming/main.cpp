6edd33d9c5ce881bcedc5cec13b5bac00f6903a1
myapit <hafeez.cs@gmail.com>
2019-10-29 12:47:52 +0800

    add windows code in cpp example

--- windows_programming/main.cpp
+++ windows_programming/main.cpp
@@ -9,7 +9,6 @@ Purpose:  sample of c++ on windows with make file. (using NMAKE)
 int main(int argc, char* argv[])
 {
   std::cout << "Selamat datang ke ..." << std::endl;
- // std::cout << "Nombor dari header file:" << mynumber << std::endl;
   displayNumber();
   std::cout << "Tekan [Enter] untuk tutup" << std::endl;
   std::cin.get();
