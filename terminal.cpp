15b4fee657de13c66f07c4c6425e14aef03b7a28
myapit <hafeez.cs@gmail.com>
2019-11-01 16:33:59 +0800

    update terminal: menu display work

--- terminal.cpp
+++ terminal.cpp
@@ -26,11 +26,24 @@ void terminal_header()
     std::cout << termcolor::red << std::setfill('=') << std::setw(100) << termcolor::reset << std::endl; 
 }
 
+void display_menu()
+{
+    std::cout << std::setfill(' ') << std::setw(23) << termcolor::bold << termcolor::magenta << "[ 0 ] Create SFTP Account Only." << termcolor::reset << std::endl; 
+    std::cout << std::setfill(' ') << std::setw(23) << termcolor::bold << termcolor::on_white << "[ 1 ] SFTP Account With Web URL." << termcolor::reset << std::endl;
+    std::cout << std::setfill(' ') << std::setw(23) << termcolor::bold << termcolor::cyan << "[ 2 ] MySQL Database Account." << termcolor::reset << std::endl;  
+
+    
+    std::cout << termcolor::red << std::setfill('=') << std::setw(100) << termcolor::reset << std::endl; 
+}
+
 int main(int argc, char *argv[])
 {
     std::cout.flush();
     std::cout << "\033[2J\033[1;1H";
+    std::cout << "\033[2J\033[1;1H";
     terminal_header();
+    display_menu();
+    std::cout << std::setfill(' ') << std::setw(23) << termcolor::bold << termcolor::cyan << "Choose an option #> " << termcolor::reset; 
     std::cin.get();
     return 0;
 }
