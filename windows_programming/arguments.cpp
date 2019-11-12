64c633c004b5f191ad9b8cbbf5e43bb19b81e0cd
myapit <hafeez.cs@gmail.com>
2019-11-12 10:04:40 +0800

    update windows cpp argument

---
+++ windows_programming/arguments.cpp
@@ -0,0 +1,18 @@
+// command_line_arguments.cpp
+// compile with: /EHsc
+// source : https://docs.microsoft.com/en-us/cpp/cpp/parsing-cpp-command-line-arguments?view=vs-2019
+#include <iostream>
+
+using namespace std;
+int main( int argc,      // Number of strings in array argv
+          char *argv[],   // Array of command-line argument strings
+          char *envp[] )  // Array of environment variable strings
+{
+    int count;
+
+    // Display each command-line argument.
+    cout << "\nCommand-line arguments:\n";
+    for( count = 0; count < argc; count++ )
+         cout << "  argv[" << count << "]   "
+                << argv[count] << "\n";
+}
